559648cfee9a2b324fdecaf8f0ceb2b047c998c3
Anuj Jagtap <jagtapanuj3@gmail.com>
2026-06-27 15:16:28 +0530

    Added resume text extraction and cleaning module

--- app/resume_parser.py
+++ app/resume_parser.py
@@ -30,11 +30,57 @@ def extract_text_from_pdf(pdf_path):
 # -------------------------
 # Testing
 # -------------------------
+#if __name__ == "__main__":
+
+#    pdf_path = "resumes/sample_resume.pdf"
+
+#    extracted_text = extract_text_from_pdf(pdf_path)
+
+#    print("\n===== EXTRACTED RESUME TEXT =====\n")
+#    print(extracted_text)
+    
+    
+import re
+import fitz
+
+def clean_resume_text(text):
+    """
+    Cleans the extracted resume text.
+
+    Args:
+        text (str): Raw extracted text.
+
+    Returns:
+        str: Cleaned text.
+    """
+
+    # Replace tabs with spaces
+    text = text.replace("\t", " ")
+
+    # Remove extra spaces
+    text = re.sub(r" +", " ", text)
+
+    # Remove multiple blank lines
+    text = re.sub(r"\n+", "\n", text)
+
+    # Remove leading and trailing spaces
+    text = text.strip()
+
+    return text
+
+#-------------------------
+# Testing
+#------------------------
+
 if __name__ == "__main__":
 
     pdf_path = "resumes/sample_resume.pdf"
 
+    # Step 1: Extract text
     extracted_text = extract_text_from_pdf(pdf_path)
 
-    print("\n===== EXTRACTED RESUME TEXT =====\n")
-    print(extracted_text)
+    # Step 2: Clean text
+    cleaned_text = clean_resume_text(extracted_text)
+
+    print("\n===== CLEANED RESUME TEXT =====\n")
+    print(cleaned_text)
